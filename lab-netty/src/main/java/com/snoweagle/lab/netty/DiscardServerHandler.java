5f1dcb400aee3a789cc62475157dac180a30e0f4
Patrick Wang <233381335@qq.com>
2017-08-21 19:09:18 +0800

    22

---
+++ lab-netty/src/main/java/com/snoweagle/lab/netty/DiscardServerHandler.java
@@ -0,0 +1,23 @@
+package com.snoweagle.lab.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by za-wangshaobo on 2017/8/21.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // Discard the received data silently.
+        ((ByteBuf) msg).release(); // (3)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
